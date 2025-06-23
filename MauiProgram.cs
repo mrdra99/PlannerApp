a84fc34c9fe11e461a5f64aea9d37c04d8c78d85
andrei-bridgetechnologies <andrei.butnaru@bridgetechnologies.it>
2025-06-23 09:39:36 +0200

    Configurazione MudBlazor: Aggiunta libreria, dipendenze, setting navmenù.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace PlannerApp
 {
@@ -15,9 +16,10 @@ namespace PlannerApp
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
 
